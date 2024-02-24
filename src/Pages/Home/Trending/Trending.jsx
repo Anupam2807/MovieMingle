5871865b9527cb13a42cae2902f81c267eb69449
bview1 <webdev.biew@gmail.com>
2024-02-24 12:55:23 +0530

    error fiexed

--- src/Pages/Home/Trending/Trending.jsx
+++ src/Pages/Home/Trending/Trending.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import ContentWrapper from "../../../components/ContentWrapper/Contentwrapper"
 import "../Home.scss"
 import SwitchTabs from '../../../Components/switchTabs/SwitchTabs';
 import useFetch from "../../../hooks/UseFetch";
@@ -21,14 +21,14 @@ const Trending = () => {
     
 
     <div className='carouselSection'>
-        <ContentWrapper>
+      <ContentWrapper>
             <span className='carouselTitle'>Trending</span>
             <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange}/>
 
         
-        </ContentWrapper>
+        
       <Carousel data={data?.results} loading={loading} endpoint={endPoint}/>
-
+      </ContentWrapper>
     </div>
   )
 }
